d1ab9ed4f956acd2b6d0097aed10d9d87dd185eb
Tom Oliver <tom@inmail.me>
2025-07-09 14:17:44 +0200

    Conditional formatting learnt

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,30 @@
+import {useState } from "react";
+
 function ListGroup() {
-    let items = [
-        "Zurich",
-        "St Gallen",
-        "Basel",
-        "Bern",
-        "Geneva"
-    ];
-    items = [];
+  let items = ["Zurich", "St Gallen", "Basel", "Bern", "Geneva"];
+
+  let selectedIndex = -1;
+  useState
 
   return (
     <>
-        <h1>List</h1>
-        {items.length===0 && <p>No item found</p>}
-        <ul className="list-group">
-            {items.map(item =>(
-                <li key={item}>{item}</li>
-                ))}
-        </ul>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {selectedIndex = index;}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
